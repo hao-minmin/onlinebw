7bb81d56900ddf0b6f4749d385f951b698aceb5a
haoxiaomin <2645627070@qq.com>
2020-05-30 23:53:39 +0800

    课程机构列表

--- bwonline/urls.py
+++ bwonline/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include,re_path
 import xadmin
 from django.views.generic import TemplateView
 from users.views import LoginView,RegisterView,ActiveUserView,ForgetPwdView,ResetView,ModifyPwdView
+from organization.views import OrgView
+
 
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
@@ -29,6 +31,7 @@ urlpatterns = [
     path('forget/',ForgetPwdView.as_view(),name='forget_pwd'),
     re_path('reset/(?P<active_code>.*)/', ResetView.as_view(), name='reset_pwd'),
     path('modify_pwd/', ModifyPwdView.as_view(), name='modify_pwd'),
+    path('org_list/',OrgView.as_view(),name = 'org_list'),
 ]
 
 
